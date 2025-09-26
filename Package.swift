9bdcdde862901829bb5233c6c8ab65601eb75bc7
pixnek <pixnek@yandex.ru>
2025-09-26 15:22:03 +0500

    fix wireguar-apple

--- Package.swift
+++ Package.swift
@@ -44,7 +44,7 @@ let package = Package(
         .package(url: "https://github.com/0xBF90E913/openssl-apple", from: "3.2.110"),
 //        .package(url: "https://git.zx2c4.com/wireguard-apple", .exact: Version("1.0.15-26")),
 //        .package(url: "https://github.com/passepartoutvpn/wireguard-apple", exact: Version("1.0.17")),
-        .package(url: "https://github.com/passepartoutvpn/wireguard-apple", revision: "b79f0f150356d8200a64922ecf041dd020140aa0")
+            .package(url: "https://git.zx2c4.com/wireguard-apple", branch: "master"),
 //        .package(url: "https://github.com/passepartoutvpn/wireguard-apple", branch: "develop")
 //        .package(name: "WireGuardKit", path: "../wireguard-apple")
     ],
